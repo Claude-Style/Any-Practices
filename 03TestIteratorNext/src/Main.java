2ebeda4ba9bc0362f8445f54269ac8d0ba4b71c5
Claude Shao <cld.shao@gmail.com>
2022-11-20 01:09:59 -0500

    commit previous any practice projects

---
+++ 03TestIteratorNext/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> listS=new ArrayList<String>();
+		listS.add("aaa");
+		listS.add("bbb");
+		listS.add("ccc");
+		
+		System.out.println("1:"+((String)(listS.iterator().next())));
+		System.out.println("2:"+((String)(listS.iterator().next())));
+		System.out.println("3:"+((String)(listS.iterator().next())));
+				
+	}
+
+}
